fc03b23fb28cc527208c33ead0556795ff53a190
DaHee Kim <87625920+awatermelonseedstudios@users.noreply.github.com>
2023-07-11 23:01:37 -0400

    added msg

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function showNext(imageToHide, imageToShow) {
-    console.log("shownext: ", imageToHide, imageToShow);
+
     if (imageToHide) {
         document.querySelector(imageToHide).style.display = "none";
     }
     if (imageToShow) {
-        document.querySelector(imageToShow).style.display = "inline";
+        document.querySelector(imageToShow).style.display = "flex";
     }
 }
